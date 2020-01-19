b75d25bfdfb7558e33ca15b94560a7d5f9a61ef5
Vishal <kulagodvishal99@gmail.com>
2020-01-19 23:08:22 +0530

    First commit

---
+++ first.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class first
+{
+static int i;
+public static void main(String[] args)
+{
+i=3;
+while(i-->0)
+{
+int i=100;
+i--;
+System.out.print(i+" ");
+}
+System.out.println();
+}
+}
