45b2ac63ae6139e44582ccdf50a49a4239e98170
Vishal <kulagodvishal99@gmail.com>
2020-01-20 10:38:40 +0530

    Second commit

--- first.java
+++ first.java
@@ -5,9 +5,11 @@ public class first
 static int i;
 public static void main(String[] args)
 {
+//global integer i
 i=3;
 while(i-->0)
 {
+//local integer i
 int i=100;
 i--;
 System.out.print(i+" ");
